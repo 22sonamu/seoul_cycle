1d76b3ef06e83d292ac87f6fbf101d65f861d476
22sonamu <22sonamu@naver.com>
2023-09-04 13:28:09 +0900

    seoul cycle project 백 초기 파일

---
+++ cycleApi/src/main/java/com/mara/cycleApi/CycleApiApplication.java
@@ -0,0 +1,13 @@
+package com.mara.cycleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleApiApplication.class, args);
+	}
+
+}
---
+++ cycleApi/src/test/java/com/mara/cycleApi/CycleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mara.cycleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
